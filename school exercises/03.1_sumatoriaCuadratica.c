a1eb863b4db9885dc15a4b5f9a8cca277171246e
Antonio Cituk <tonycituk@gmail.com>
2021-04-12 09:11:56 -0500

    12-04-21 :heavy_minus_sign:

--- school exercises/03.1_sumatoriaCuadratica.c
+++ school exercises/03.1_sumatoriaCuadratica.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#include <math.h>
 //Calcular la sumatoria de i=1 hasta 10 de i^2
 
 int main()
 {
-    double i = 1.0, sumatoria = 0;
+    double i = 1, sumatoria = 0;
+    
     while (i <= 10)
     {
-        sumatoria+=pow(i,2);
-        //printf("%0.0lf^2 = %0.0lf\n", i, pow(i, 2.0));
+        sumatoria+=i*i;
+        //printf("%0.0lf^2 = %0.0lf\n", i, i*i);
         i++;
     }
 
